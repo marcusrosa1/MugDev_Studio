f77f4090d640a6ca89d8efea137ea628d480a7cf
MarcusRosa <marcus.rosa25@gmail.com>
2026-01-03 18:43:45 -0300

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,29 @@
-// MENU MOBILE
-const menuToggle = document.getElementById("menuToggle");
-const navMenu = document.getElementById("navMenu");
+const menuToggle = document.querySelector(".menu-toggle");
+const navMenu = document.querySelector(".nav");
+const navLinks = document.querySelectorAll(".nav a");
+const backToTop = document.getElementById('backToTop');
 
 menuToggle.addEventListener("click", () => {
   navMenu.classList.toggle("active");
 });
 
-// SCROLL SUAVE
-const links = document.querySelectorAll('nav a');
-
-links.forEach(link => {
-  link.addEventListener("click", () => {
-    navMenu.classList.remove("active");
-  });
+navLinks.forEach(link => {
+    link.addEventListener("click", () => {
+        navMenu.classList.remove("active");
+    });
 });
 
-// ANIMAÇÃO AO SCROLL
-const sections = document.querySelectorAll("section");
-
-const observer = new IntersectionObserver(entries => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add("show");
+window.addEventListener('scroll', () =>{
+    if (window.scrollY >300) {
+        backToTop.style.display = 'block';        
+    } else {
+        backToTop.style.display = 'none';
     }
-  });
-}, {
-  threshold: 0.2
 });
 
-sections.forEach(section => {
-  section.classList.add("hidden");
-  observer.observe(section);
+backToTop.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
 });
