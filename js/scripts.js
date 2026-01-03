655eb1fc99ca97c5252b74254fbef8f8b19d7dd7
MarcusRosa <marcus.rosa25@gmail.com>
2026-01-02 23:57:35 -0300

    Refactor mobile menu and scroll animation logic
    Updated menu toggle and scroll animation functionality.

--- js/scripts.js
+++ js/scripts.js
@@ -3,32 +3,32 @@ const menuToggle = document.getElementById("menuToggle");
 const navMenu = document.getElementById("navMenu");
 
 menuToggle.addEventListener("click", () => {
-    navMenu.classList.toggle("activate");
+  navMenu.classList.toggle("active");
 });
 
-//SCROLL SUAVE
-const links = documentquerySelectorAll('nav a');
+// SCROLL SUAVE
+const links = document.querySelectorAll('nav a');
 
 links.forEach(link => {
-    links.addEventListener("click", () =>{
-        navMenu.classList.remove("activate");
-})
-})
+  link.addEventListener("click", () => {
+    navMenu.classList.remove("active");
+  });
+});
 
-//ANIMAÇÃO AO SCROLL
+// ANIMAÇÃO AO SCROLL
 const sections = document.querySelectorAll("section");
 
 const observer = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add("show");
-        }
-    });
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    }
+  });
 }, {
-    threshold:2.0
+  threshold: 0.2
 });
 
 sections.forEach(section => {
-    section.classList.add("hidden");
-    observer.observe(section);
+  section.classList.add("hidden");
+  observer.observe(section);
 });
